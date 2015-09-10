14fe686eea5ec86dc9827ac5decac282d712992e
talumbau <tj.alumbaugh@continuum.io>
2015-09-10 08:12:13 -0500

    fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ wrapper = Extension('my_wrapper', sources=['fib1.f'], libraries=['my_module'])
 
 setup(
     name='fortranwrap',
-    version='0.1'
+    version='0.1',
     libraries = [('my_module', dict(sources=['fib1.f'],
                                     extra_f90_compile_args=["-ffixed-form"]))],
     ext_modules = [wrapper]
