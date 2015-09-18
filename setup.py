6ac20356dd5aa38663f0116d1f5a17efc9c3a444
talumbau <tj.alumbaugh@continuum.io>
2015-09-18 08:56:13 -0500

    update to version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ wrapper = Extension('fwrapper', sources=['fib1.f'], libraries=['my_module'])
 
 setup(
     name='fortranwrap',
-    version='0.1',
+    version='0.2',
     libraries = [('my_module', dict(sources=['fib1.f'],
                                     extra_f90_compile_args=["-ffixed-form"]))],
     ext_modules = [wrapper]
